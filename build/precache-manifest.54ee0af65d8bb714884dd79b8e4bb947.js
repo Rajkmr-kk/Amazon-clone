f082ced7af75e16cff50cc1f611bab2e08e4fd18
Rajkumar <rajkmrbtech@gmail.com>
2020-09-27 13:48:50 +0530

    final commit

---
+++ build/precache-manifest.54ee0af65d8bb714884dd79b8e4bb947.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ce54ac4a7b942945af79462e0d48de7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b28a4dc4dae5861b492",
+    "url": "/static/css/main.78c7c615.chunk.css"
+  },
+  {
+    "revision": "26da86e65134d6fd770b",
+    "url": "/static/js/2.c400e35a.chunk.js"
+  },
+  {
+    "revision": "2b587c35db5df3608932eb682dfa83dc",
+    "url": "/static/js/2.c400e35a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b28a4dc4dae5861b492",
+    "url": "/static/js/main.5cc82ac7.chunk.js"
+  },
+  {
+    "revision": "92a146e5ab9e0f9e785b",
+    "url": "/static/js/runtime-main.90140090.js"
+  }
+]);
